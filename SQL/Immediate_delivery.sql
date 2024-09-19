ebd470e41634957f8fb3175d43801ee34a278639
Hassan Riaz <123826636+hassandsriaz@users.noreply.github.com>
2024-09-19 21:59:42 +0500

    Create Immediate_delivery.sql

---
+++ SQL/Immediate_delivery.sql
@@ -0,0 +1,9 @@
+WITH first_orders AS (
+    SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM delivery
+    GROUP BY customer_id
+)
+SELECT 
+    ROUND(SUM(CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS immediate_percentage
+FROM delivery d
+JOIN first_orders fo ON d.customer_id = fo.customer_id AND d.order_date = fo.first_order_date;
