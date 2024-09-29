3b29d3c65b76f4e24cbe712e8180083d17f482a1
Hassan Riaz <123826636+hassandsriaz@users.noreply.github.com>
2024-09-29 23:23:28 +0500

    29/9/2024 (1)

---
+++ Python/prefix_sum.py
@@ -0,0 +1,8 @@
+# Rutime: 17ms | Beats: 86.02% (Depending on Connection) 
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        print(nums)
